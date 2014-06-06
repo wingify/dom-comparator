d4b63f88d2a185cf29206cb7bccedc3fb18d3e46
Himanshu Kapoor <himanshu.kapoor@wingify.com>
2014-06-06 16:30:31 +0530

    Add jshint task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,7 @@ module.exports = function (grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
