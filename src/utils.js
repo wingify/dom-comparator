a37e533f562aea969993b8a2e724ce83f709ea7c
Himanshu Kapoor <himanshu.kapoor@wingify.com>
2014-06-09 13:00:15 +0530

    Fix Utils cache.

--- src/utils.js
+++ src/utils.js
@@ -5,14 +5,14 @@ VWO.FunctionUtils = {
 		var _cache = {};
 		function cached() {
 			cached.isCached = true;
-			var args = Array.prototype.apply(null, arguments);
+			var args = [].slice.apply(arguments);
 			/* jshint -W040 */ // no strict mode violation here, its needed
-			return _cache[args] || (_cache[args] = fn.apply(this, [].slice.apply(arguments)));
+			return _cache[args] || (_cache[args] = fn.apply(this, args));
 		}
 		cached.fn = fn;
 		cached.uncache = function () {
 			_cache = {};
 		};
 		return cached;
-	};
+	}
 }
