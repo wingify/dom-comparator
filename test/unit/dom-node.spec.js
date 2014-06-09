205bb0a87016a244d546da1e2ad17fb78a8dac51
Himanshu Kapoor <himanshu.kapoor@wingify.com>
2014-06-09 13:00:24 +0530

    Add a sample test case.

--- test/unit/dom-node.spec.js
+++ test/unit/dom-node.spec.js
@@ -1,7 +1,11 @@
 describe('module: DOMNode', function () {
-	describe('method: someMethod', function () {
-		it('should pass', function () {
-			expect(true).toBe(true);
+	describe('method: nodeName', function () {
+		it('gets the name of the node', function () {
+			var domNode = VWO.DOMNode.create({
+				el: $('<div><span>span content</span><ul><li>item 1</li><li>item 2</li></ul></div>').get(0)
+			});
+
+			expect(domNode.nodeName()).toBe('DIV');
 		});
 	});
 })
