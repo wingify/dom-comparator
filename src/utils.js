6ef1b292bd8d4a784e2b045d948650acde6ce47e
Himanshu Kapoor <himanshu.kapoor@wingify.com>
2014-06-11 20:09:43 +0530

    utils: Add String and Number host extensions.

--- src/utils.js
+++ src/utils.js
@@ -19,6 +19,123 @@ VWO.FunctionUtils = {
 }
 
 /**
+ * @class
+ * @name String
+ */
+
+/**
+ * Trims any leading and trailing spaces and returns a new string.
+ *
+ * @return {String} The trimmed string
+ */
+String.prototype.trim = function () {
+  return this.rtrim().ltrim();
+};
+
+/**
+ * Trims any leading spaces and returns a new string.
+ *
+ * @return {String} The trimmed string
+ */
+String.prototype.ltrim = function () {
+  return this.replace(/^[ \r\n]*/, '');
+};
+
+/**
+ * Trims any trailing spaces and returns a new string.
+ *
+ * @return {String} The trimmed string
+ */
+String.prototype.rtrim = function () {
+  return this.replace(/[ \n\r]*$/, '');
+};
+
+/**
+ * Finds the nth index of a needle in a haystack.
+ *
+ * @param  {String} needle The string to search for.
+ * @param  {Number} index  Index where to begin the search
+ * @return {Number}        The nth index of the string.
+ */
+String.prototype.nthIndexOf = function (needle, index) {
+  var pos = this.indexOf(needle, 0);
+  while (index-- > 0 && pos !== -1) {
+    pos = this.indexOf(needle, pos + 1);
+  }
+  return pos;
+};
+
+var STR_PAD_LEFT = 1;
+var STR_PAD_RIGHT = 2;
+var STR_PAD_BOTH = 3;
+
+String.prototype.lpad = function (len, pad) {
+  return this.pad(len, pad, STR_PAD_LEFT);
+};
+
+String.prototype.rpad = function (len, pad) {
+  return this.pad(len, pad, STR_PAD_RIGHT);
+};
+
+String.prototype.pad = function pad(len, pad, dir) {
+  var str = this;
+
+  if (typeof(len) == "undefined") { len = 0; }
+  if (typeof(pad) == "undefined") { pad = ' '; }
+  if (typeof(dir) == "undefined") { dir = STR_PAD_RIGHT; }
+
+  if (len + 1 >= str.length) {
+    switch (dir) {
+      case STR_PAD_LEFT:
+        str = new Array(len + 1 - str.length).join(pad) + str;
+        break;
+
+      case STR_PAD_BOTH:
+        var padlen = len - str.length;
+        var right = Math.ceil(padlen / 2);
+        var left = padlen - right;
+        str = new Array(left+1).join(pad) + str + new Array(right+1).join(pad);
+        break;
+
+      default:
+        str = str + new Array(len + 1 - str.length).join(pad);
+        break;
+    }
+  }
+  return str;
+};
+
+/**
+ * @class
+ * @name Number
+ */
+
+/**
+ * Makes sure the number is within a specified range. If not, it
+ * returns the upper or lower limit of that range.
+ *
+ * @param {Number} [min] The lower limit of the range to clamp in.
+ * @param {Number} [max] The upper limit of the range to clamp in.
+ * @return {Number}
+ */
+Number.prototype.clamp = function (min, max) {
+  if (typeof min !== 'number') min = -Infinity;
+  if (typeof max !== 'number') max = Infinity;
+  return Math.max(min, Math.min(max, this));
+};
+
+/**
+ * Checks if the number is within a range.
+ *
+ * @param {Number} min The lower limit of the range.
+ * @param {Number} max The upper limit of the range.
+ * @return {Boolean}
+ */
+Number.prototype.isWithinRange = function (min, max) {
+  return this > min && this < max;
+};
+
+/**
  * The nodeName of this element.
  *
  * @memberOf jQuery_fn
